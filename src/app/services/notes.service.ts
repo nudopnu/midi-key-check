e0e0e30bb9bad5c8dac33fa464ac6750b89f415a
nudopnu <forsuse@live.de>
2024-06-09 14:10:19 +0200

    Added persistence for chosen key

--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -18,9 +18,26 @@ export class NotesService {
     key: signal("C" as PitchName),
   };
 
+  constructor() {
+    const key = this.load('key');
+    if (!key) return;
+    this.set('key', key);
+  }
+
   set<T extends keyof typeof this._state>(state: T, value: WritableSignalValue<ReturnType<Signal<(typeof this._state)>>[T]>) {
     const newLocal = this._state[state] as WritableSignal<WritableSignalValue<ReturnType<Signal<(typeof this._state)>>[T]>>;
     newLocal.set(value);
+    if (state !== 'key') return;
+    this.persist(state, value);
+  }
+
+  private persist<T extends keyof typeof this._state>(key: T, value: WritableSignalValue<ReturnType<Signal<(typeof this._state)>>[T]>) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  private load<T extends keyof typeof this._state>(key: T): WritableSignalValue<ReturnType<Signal<(typeof this._state)>>[T]> | undefined {
+    const stringValue = localStorage.getItem(key);
+    return stringValue && JSON.parse(stringValue);
   }
 
 }
